61173b2bbfee26fe07d67c2766280c66bf950d67
Raghav Heda <raghav.heda@elastic.run>
2022-01-22 20:56:06 +0530

    Dialog Component Completed

--- Dialog Component/dialogComp.js
+++ Dialog Component/dialogComp.js
@@ -6,8 +6,13 @@ const btnSimple = document.querySelector(".simple-btn")
 const simpleDialog = document.querySelector(".show-dialog-simple")
 const mainSimple = document.querySelector(".main-div-simple")
 
+const btnConfirmation = document.querySelector(".confirmation-btn")
+const confirmationDialog = document.querySelector(".show-dialog-confirmation")
+const mainConfirmation = document.querySelector(".main-div-confirmation")
+
 btnAlert.addEventListener("click",showalertDialog);
 btnSimple.addEventListener("click",showsimpleDialog);
+btnConfirmation.addEventListener("click",showconfirmationDialog);
 
 function showalertDialog(){
     console.log("Hi")
@@ -21,11 +26,20 @@ function showsimpleDialog(){
     mainSimple.style.display="block"
 }
 
+function showconfirmationDialog(){
+    console.log("Hi")
+    confirmationDialog.style.visibility="visible"
+    mainConfirmation.style.display="block"
+}
+
 window.onclick = function(event) {
     if (event.target == mainAlert) {
         mainAlert.style.display = "none";
     }
     if (event.target == mainSimple) {
         mainSimple.style.display = "none";
+    }
+    if (event.target == mainConfirmation) {
+        mainConfirmation.style.display = "none";
     }
   }
